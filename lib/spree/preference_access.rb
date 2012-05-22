1408626a169c6a025afad577d5d5e5408ed963f8
David Guthu <davidguthu@gmail.com>
2012-05-22 14:43:12 -0400

    memoize prefs to improve perf

--- lib/spree/preference_access.rb
+++ lib/spree/preference_access.rb
@@ -6,9 +6,9 @@ module Spree::PreferenceAccess
         key = key.to_s if key.is_a?(Symbol)
         return nil unless config = self.instance
         # preferences will be cached under the name of the class including this module (ex. Spree::Config)
-        prefs = Rails.cache.fetch("configuration_#{config.class.name}".to_sym) { config.preferences }
-        return prefs if key.nil?
-        prefs[key]
+        @prefs ||= Rails.cache.fetch("configuration_#{config.class.name}".to_sym) { config.preferences }
+        return @prefs if key.nil?
+        @prefs[key]
       end
 
       # Set the preferences as specified in a hash (like params[:preferences] from a post request)
